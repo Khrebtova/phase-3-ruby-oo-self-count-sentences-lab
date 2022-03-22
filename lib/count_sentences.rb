b5fd42dcf79d80fc07ba1872952930c851f134e3
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-03-22 11:23:59 -0700

    commit tree d050f1f1ddd58d63ff33765dabba2502b7bca0f4

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.match(/[.]\z/) ? true : false
   end
 
   def question?
-
+    self.match(/[?]\z/) ? true : false
   end
 
   def exclamation?
-
+    self.match(/[!]\z/) ? true : false
   end
 
   def count_sentences
-
+    self.scan(/[.!?]+/).length
   end
 end
